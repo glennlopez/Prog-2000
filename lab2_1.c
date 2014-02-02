e6ae427d641966c8cf3af092f5aac4fca65cde5d
Glenn Lopez <glennlopez@gmail.com>
2014-02-01 19:09:26 -0600

    comments
    edited comments

--- lab2_1.c
+++ lab2_1.c
@@ -3,9 +3,9 @@ Program:			Lab 2.1
 Programmer:			Glenn Lopez
 Date:				2014-02-01 16:33
 Class:				E12
-Description:		Uses lbs of port A for output / altfunc is disabled
+Description:		Uses the 3 led's on zilog for output
 Github page:		https://github.com/glennlopez/lab2_1/blob/master/lab2_1.c
-					(visit the link above for the latest code revision)		
+	(visit the link above for the latest code history & revision)		
 ---------------------------------------------------------------------------------*/
 
 //header files
@@ -25,18 +25,18 @@ void main(void) //!FLAG#1 - comment out if function requires return 0;
 	PACTL = 0x00;				//<-- 
 
 	//data directions
-	PAADDR = 0x01;				//<--
-	PACTL = 0xF8;				//<--
+	PAADDR = 0x01;				//<-- write cycle into PAADDR (0x0FDO)
+	PACTL = 0xF8;				//<-- 
 
 	//output controll
-	PAADDR = 0x03;				//<--
+	PAADDR = 0x03;				//<-- write cycle into PAADDR (0x0FDO)
 	PACTL = 0x00;				//<--
 
 	//prevent inadvertant changes to the sub-registers
 	PAADDR = 0x00;				//<--
 
 	//assign local variable 'leds' to PAOUT constant
-	PAOUT = leds;
+	PAOUT = leds;				//<-- 
 
 /*-- 			END MAIN FUNCTION 			--*/
 //return 0; //!FLAG#1 - comment out if int main(void) is not in use
