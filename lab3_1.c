2aab7f66ae8fb180cd8829722354004aed6dcb24
Glenn Lopez <glennlopez@gmail.com>
2014-02-06 08:31:11 -0600

    complete rebuild of master

--- lab3_1.c
+++ lab3_1.c
@@ -17,62 +17,65 @@ Github page:		https://github.com/glennlopez/Prog-2000/blob/master/lab3_1.c
 
 
 /*-- 			START HEADER FILES 			--*/
-//#include <stdio.h>				//<-- Standard Input and Output library for C
-#include <ez8.h>					//<-- library for int1erfacing with Z8
-//#include<myFunctions.h>			//<-- library for future subroutines and identifiers
+ //#include <stdio.h>								//<-- Standard Input and Output library for C
+ #include <ez8.h>									//<-- library for int1erfacing with Z8
+ //#include<myFunctions.h>							//<-- library for future subroutines and identifiers
 /*-- 			END HEADER FILES 			--*/
 
 
-/*-- 			START MAIN FUNCTION 			--*/
-int main(void) //!FLAG#1 - comment out if function does not require return 0;  
-//void main(void) //!FLAG#1 - comment out if function requires return 0;	
-{
+/*-- 			START MAIN FUNCTION 			--*/ 
+ int main(void)//!FLAG#1 - comment out if function does not require return 0;  
+ //void main(void) //!FLAG#1 - comment out if function requires return 0;	
+ {
 	//variable declarations and assignments;
-	unsigned char leds=0x02; 		//<-- makes 0x02 = leds
-	unsigned char push_button;		//<-- makes 0x00 = push_button
-	push_button=0x00;
+	unsigned char leds = 0x02; 						//<-- makes 0x02 = leds an unsigned char
+	unsigned char push_button;						//<-- makes 0x00 = push_button an unsigned char
+	push_button = 0x00;
+			
+	/*- PORT A CONFIGURATION -*/
+	 //alternate function 
+	 PAADDR = 0x02;									//<-- write cycle into PAADDR
+	 PACTL = 0x00;									//<-- disable altfuctions
 
-/*- PORT A CONFIGURATION -*/
+	 //data directions
+	 PAADDR = 0x01;									//<-- write cycle into PAADDR
+	 PACTL = 0xF8;									//<-- set pins 2-0 as outputs and 7-3 as inputs
 
-	//alternate function 
-	PAADDR = 0x02;					//<-- write cycle into PAADDR
-	PACTL = 0x00;					//<-- disable altfuctions
+	 //output controll
+	 PAADDR = 0x03;									//<-- write cycle into PAADDR
+	 PACTL = 0x00;									//<-- makes all outputs push-pull
 
-	//data directions
-	PAADDR = 0x01;					//<-- write cycle into PAADDR
-	PACTL = 0xF8;					//<-- set pins 2-0 as outputs and 7-3 as inputs
+	 //preventative care 
+	 PAADDR = 0x00; 								//<-- prevents any changes to the subregs
 
-	//output controll
-	PAADDR = 0x03;					//<-- write cycle into PAADDR
-	PACTL = 0x00;					//<-- makes all outputs push-pull
+	/*- PORT C CONFIGURATION -*/
+	 //alternate function
+	 PCADDR = 0x02;									//<-- write cyle to port PCADDR
+	 PCCTL = 0x00;									//<-- 
 
-	//preventative care 
-	PAADDR = 0x00; 					//<-- prevents any changes to the subregs
-	
-/*- PORT C CONFIGURATION -*/
-	PCADDR = 0x02;					//<-- 
-	PCCTL = 0x00;					//<-- 
+	 //data directions
+	 PCADDR = 0x01;									//<-- 
+	 PCCTL = 0x01; 									//<-- 
 
-	PCADDR = 0x01;					//<-- 
-	PCCTL = 0x01; 					//<-- 
-
-	//preventative care
-	PCADDR = 0x00;					//<-- prevents and changes to the subregs
-	
-	/*-- 			START BUTTON STATEMENT 			--*/
-	PAOUT=leds;
-	while (1)
-	{
-		push_button = PCIN;					//<-- 				
-		push_button = push_button & 0x01;	//<-- 
-		if(push_button == 0x00)				//<-- 
+	 //preventative care
+	 PCADDR = 0x00;									//<-- prevents and changes to the subregs
+		
+	/*-- PUSH BUTTON --*/
+	 PAOUT=leds;
+	 while (1)
+	 {
+		push_button = PCIN;							//<-- makes PCIN equal push_button				
+		push_button = push_button & 0x01;			//<-- 
+		if(push_button == 0x00)						//<-- true if push_button is equal to 0x00
 		{
 			leds=leds^0x07;							//<-- 
-			PAOUT=leds;								//<-- 
+			PAOUT=leds;
 		}
-	}
-	/*-- 			END BUTTON IF STATEMENT 			--*/
+	 }
 
-return 0; //!FLAG#1 - comment out if int main(void) is not in use
-}
+	return 0; //!FLAG#1 - comment out if int main(void) is not in use
+ }
 /*-- 			END MAIN FUNCTION 			--*/
+
+// experimental builds available for review at
+// https://github.com/glennlopez/Prog-2000/tree/experimental
