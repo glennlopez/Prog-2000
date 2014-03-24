265ffb53799a44a2576cccca1c37669b12169101
Glenn Lopez <glennlopez@gmail.com>
2014-03-24 15:44:38 -0500

    Update lab5_3.c

--- lab5_3.c
+++ lab5_3.c
@@ -26,19 +26,20 @@ Github page:		https://github.com/glennlopez/Prog-2000/
  	unsigned char val;
  	unsinged char count = 0;
  	
- 	PEADDR = 0X02;
- 	PECTL = 0X00;
- 	PEADDR = 0X01;
- 	PECTL = 0X00;
- 	PEADDR = 0X03;
- 	PECTL = 0X00;
- 	PEADDR = 0X00;
- 	
- 	PDADDR = 0X02;
- 	PDCTL = 0X00;
- 	PDADDR = 0X01;
- 	PDCTL = 0XFF;
- 	PDADDR = 0X00;
+ 	//*-- Initialize ports *--//
+ 	PEADDR = 0X02;	//<-- alt func
+ 	PECTL = 0X00;	//<-- disable alt func
+ 	PEADDR = 0X01;	//<-- data address
+ 	PECTL = 0X00;	//<-- set to output
+ 	PEADDR = 0X03;	//<-- controll function (output)
+ 	PECTL = 0X00;	//<-- set pushpull
+ 	PEADDR = 0X00;	//<-- prevents changes to sub-register
+ 
+ 	PDADDR = 0X02;	//<-- alt func
+ 	PDCTL = 0X00;	//<-- disable alt func
+ 	PDADDR = 0X01;	//<-- data address
+ 	PDCTL = 0XFF;	//<-- set to input
+ 	PDADDR = 0X00;	//<-- preven changes to sub-registers
  	
  	while(1)
  	{
